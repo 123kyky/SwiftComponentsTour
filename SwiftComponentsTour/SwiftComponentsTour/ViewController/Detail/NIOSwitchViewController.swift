027bf02f5f1e38e903a74d7e163aa28a744af393
123kyle.roberts <123kyle.roberts@gmail.com>
2015-07-07 11:50:01 -0500

    Switches work

--- SwiftComponentsTour/SwiftComponentsTour/ViewController/Detail/NIOSwitchViewController.swift
+++ SwiftComponentsTour/SwiftComponentsTour/ViewController/Detail/NIOSwitchViewController.swift
@@ -10,26 +10,86 @@ import UIKit
 
 class NIOSwitchViewController: UIViewController {
 
+    @IBOutlet weak var lightSwitch: UISwitch!
+    @IBOutlet weak var brightsButton: UIButton!
+    @IBOutlet weak var lightsLabel: UILabel!
+    
+    @IBOutlet weak var leftSpotlight: UIImageView!
+    @IBOutlet weak var leftSpotlightLeftConstraint: NSLayoutConstraint!
+    @IBOutlet weak var rightSpotlight: UIImageView!
+    @IBOutlet weak var rightSpotlightRightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.brightsButton.selected = false
+        self.lightsLabel.layer.shadowOpacity = 1
+        self.lightsLabel.layer.shadowRadius = 6
+        self.lightsLabel.layer.shadowColor = UIColor.whiteColor().CGColor
+        
+        self.rightSpotlight.transform = CGAffineTransformMakeScale(-1, 1)
+        self.leftSpotlightLeftConstraint.constant = -self.leftSpotlight.frame.size.width
+        self.rightSpotlightRightConstraint.constant = -self.rightSpotlight.frame.size.width
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    
+    @IBAction func spotlightsToggled(sender: AnyObject) {
+        if self.lightSwitch.on {
+            self.turnOnSpotlights()
+        } else {
+            self.turnOffSpotlights()
+        }
     }
-    */
+
+    @IBAction func houseLightsToggled(sender: AnyObject) {
+        if self.brightsButton.selected {
+            self.brightsButton.selected = false
+            self.turnOffHouseLights()
+        } else {
+            self.brightsButton.selected = true
+            self.turnOnHouseLights()
+        }
+    }
+    
+    // MARK: - Animations
+    
+    func turnOnSpotlights() {
+        if self.brightsButton.selected {
+            self.houseLightsToggled(self)
+        }
+        
+        self.leftSpotlightLeftConstraint.constant = 0
+        self.rightSpotlightRightConstraint.constant = 0
+        
+        UIView .animateWithDuration(0.75, animations: { () -> Void in
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func turnOffSpotlights() {
+        self.leftSpotlightLeftConstraint.constant = -self.leftSpotlight.frame.size.width
+        self.rightSpotlightRightConstraint.constant = -self.rightSpotlight.frame.size.width
+        
+        UIView .animateWithDuration(0.75, animations: { () -> Void in
+            self.view.layoutIfNeeded()
+            self.lightSwitch.on = false
+        })
+    }
+    
+    func turnOnHouseLights() {
+        self.turnOffSpotlights()
+        UIView .animateWithDuration(0.75, animations: { () -> Void in
+            self.view.backgroundColor = UIColor.whiteColor()
+            self.lightsLabel.layer.shadowColor = UIColor.blackColor().CGColor
+        })
+    }
+    
+    func turnOffHouseLights() {
+        UIView .animateWithDuration(0.75, animations: { () -> Void in
+            self.view.backgroundColor = UIColor.blackColor()
+            self.lightsLabel.layer.shadowColor = UIColor.whiteColor().CGColor
+        })
+    }
 
 }
