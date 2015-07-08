3b2559bbd1eadc2c97f2028ae1048454e3a33030
123kyle.roberts <123kyle.roberts@gmail.com>
2015-07-08 14:11:47 -0500

    Stepper work.

--- SwiftComponentsTour/SwiftComponentsTour/ViewController/Detail/NIOStepperViewController.swift
+++ SwiftComponentsTour/SwiftComponentsTour/ViewController/Detail/NIOStepperViewController.swift
@@ -8,28 +8,89 @@
 
 import UIKit
 
-class NIOStepperViewController: UIViewController {
+class NIOStepperViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var numberStepper: UIStepper!
+    @IBOutlet weak var dateStepper: UIStepper!
+    @IBOutlet weak var utilityStepper: UIStepper!
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var utilitySegmentedControl: UISegmentedControl!
+    
+    var dateFormatter = NSDateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.dateFormatter.dateStyle = .FullStyle
+        self.dateLabel.text = self.dateFormatter.stringFromDate(NSDate())
+        
+        let calendar: NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        let components: NSDateComponents = calendar.components(.CalendarUnitDay, fromDate: NSDate())
+        self.dateStepper.value = Double(components.day)
+        
+        self.numberStepper.maximumValue = DBL_MAX
+        self.numberStepper.minimumValue = -DBL_MAX
+        self.utilityStepper.value = Double(UIScreen.mainScreen().brightness)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func stepperChangedValue(sender: AnyObject) {
+        if sender as! NSObject == self.numberStepper {
+            self.adjustNumberValue()
+        } else if sender as! NSObject == self.dateStepper {
+            self.adjustDate()
+        } else if sender as! NSObject == self.utilityStepper {
+            self.adjustBrightness()
+        }
     }
-    */
+    
+    @IBAction func numberTextFieldDidChange(sender: AnyObject) {
+        self.numberStepper.value = (self.numberTextField.text as NSString).doubleValue
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        if string == "" { // backspace
+            return true
+        }
+        
+        let characterSet = NSCharacterSet(charactersInString: "0123456789")
+        for unichar in string.utf16 {
+            if  characterSet.characterIsMember(unichar) {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func adjustNumberValue() {
+        self.numberTextField.text = "\(Int(self.numberStepper!.value))"
+    }
+    
+    func adjustDate() {
+        let date = self.dateFormatter.dateFromString(self.dateLabel!.text!)
+        var components: NSDateComponents = NSCalendar.currentCalendar().components(.CalendarUnitDay, fromDate: date!)
+        
+        var steppedDate: NSDate? = nil
+        if components.day > Int(self.dateStepper.value) {
+            steppedDate = NSCalendar.currentCalendar().dateByAddingUnit(.CalendarUnitDay, value: -1, toDate: date!, options: .SearchBackwards)
+        } else {
+            steppedDate = NSCalendar.currentCalendar().dateByAddingUnit(.CalendarUnitDay, value: 1, toDate: date!, options: nil)
+        }
+
+        components = NSCalendar.currentCalendar().components(.CalendarUnitDay, fromDate: steppedDate!)
+        
+        self.dateStepper.value = Double(components.day)
+        self.dateLabel.text = self.dateFormatter.stringFromDate(steppedDate!)
+    }
+    
+    func adjustBrightness() {
+        UIScreen.mainScreen().brightness = CGFloat(self.utilityStepper.value)
+    }
 
 }
