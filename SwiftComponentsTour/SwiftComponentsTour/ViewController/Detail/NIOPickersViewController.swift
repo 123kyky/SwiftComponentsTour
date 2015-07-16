60f06c4cd37bc0da144f46210d469945a6a832fe
123kyle.roberts <123kyle.roberts@gmail.com>
2015-07-16 09:20:25 -0500

    Picker work

--- SwiftComponentsTour/SwiftComponentsTour/ViewController/Detail/NIOPickersViewController.swift
+++ SwiftComponentsTour/SwiftComponentsTour/ViewController/Detail/NIOPickersViewController.swift
@@ -8,28 +8,69 @@
 
 import UIKit
 
-class NIOPickersViewController: UIViewController {
-
+class NIOPickersViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var pickerDisplayLabel: UILabel!
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    var pickerData = ["Mountain View", "Sunnyvale", "Cupertino", "Santa Clara", "San Jose"]
+    var dateFormatter = NSDateFormatter()
+    var datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+        self.setUpPicker()
+        self.setUpDatePicker()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - UIPickerView
+    
+    func setUpPicker() {
+        self.picker.layer.borderColor = UIColor.blackColor().CGColor
+        self.picker.layer.borderWidth = 2
+        
+        self.picker.dataSource = self
+        self.picker.delegate = self
+        
+        self.picker.selectRow(2, inComponent: 0, animated: false)
+        self.pickerDisplayLabel.text = self.pickerData[2]
     }
-    */
-
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 5
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return self.pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.pickerDisplayLabel.text = self.pickerData[row]
+    }
+    
+    // MARK: - UIDatePicker
+    
+    func setUpDatePicker() {
+        self.datePicker.datePickerMode = .Date;
+        self.datePicker.addTarget(self, action: Selector("datePickerDateChanged"), forControlEvents: .ValueChanged)
+        self.datePicker.date = NSDate()
+        self.datePicker.backgroundColor = UIColor.whiteColor()
+        self.dateTextField.inputView = self.datePicker
+        self.dateFormatter.dateStyle = .FullStyle
+    }
+        
+    func datePickerDateChanged() {
+        self.dateTextField.text = self.dateFormatter.stringFromDate(self.datePicker.date)
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        super.touchesBegan(touches, withEvent: event)
+        
+        self.view.endEditing(true)
+    }
 }
