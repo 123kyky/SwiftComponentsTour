4873a634e0b4d2cc911b89b9d8dbab6318794ac0
123kyle.roberts <123kyle.roberts@gmail.com>
2015-07-16 08:52:49 -0500

    Progress bar work

--- SwiftComponentsTour/SwiftComponentsTour/ViewController/Detail/NIOProgressViewController.swift
+++ SwiftComponentsTour/SwiftComponentsTour/ViewController/Detail/NIOProgressViewController.swift
@@ -10,26 +10,48 @@ import UIKit
 
 class NIOProgressViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var isLoading = false
+    var timer: NSTimer!
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+        if !self.isLoading {
+            self.activityIndicator.startAnimating()
+            self.startProgress()
+            self.startButton.setTitle("Stop", forState: .Normal)
+        } else {
+            self.activityIndicator.stopAnimating()
+            self.stopProgress()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func startProgress() {
+        if self.progressBar.progress == 1 {
+            self.progressBar.progress = 0
+        }
+        
+        self.isLoading = true
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("incrementProgressBar"), userInfo: nil, repeats: true)
     }
-    */
-
+    
+    func incrementProgressBar() {
+        self.progressBar.progress += 0.001
+        if (self.progressBar.progress == 1) {
+            self.activityIndicator.stopAnimating()
+            self.stopProgress()
+        }
+    }
+    
+    func stopProgress() {
+        self.timer.invalidate()
+        self.isLoading = false
+        if self.progressBar.progress == 1 {
+            self.startButton.setTitle("Start", forState: .Normal)
+        } else {
+            self.startButton.setTitle("Continue", forState: .Normal)
+        }
+    }
 }
