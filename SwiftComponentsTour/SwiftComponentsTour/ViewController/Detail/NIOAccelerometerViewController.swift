7d9f526b16ebe054aeafe9a6d402c67278ada6c3
123kyle.roberts <123kyle.roberts@gmail.com>
2015-07-17 10:22:29 -0500

    Base accelerometer work.

--- SwiftComponentsTour/SwiftComponentsTour/ViewController/Detail/NIOAccelerometerViewController.swift
+++ SwiftComponentsTour/SwiftComponentsTour/ViewController/Detail/NIOAccelerometerViewController.swift
@@ -7,29 +7,55 @@
 //
 
 import UIKit
+import CoreMotion
 
 class NIOAccelerometerViewController: UIViewController {
 
+    @IBOutlet weak var xValueLabel: UILabel!
+    @IBOutlet weak var yValueLabel: UILabel!
+    @IBOutlet weak var zValueLabel: UILabel!
+    @IBOutlet weak var functionalitySwitch: UISwitch!
+    
+    var motionManager = CMMotionManager()
+    var numberFormatter = NSNumberFormatter()
+    var calibratedAcceleration = CMAcceleration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        self.numberFormatter.numberStyle = .DecimalStyle
+        self.numberFormatter.maximumFractionDigits = 3
+        
+        if self.motionManager.accelerometerAvailable {
+            self.setUpAccelerometerMonitoring()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setUpAccelerometerMonitoring() {
+        self.motionManager.accelerometerUpdateInterval = 0.01
+        self.motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue()) {
+            [weak self] (data: CMAccelerometerData!, error: NSError!) in
+            self!.xValueLabel.text = self?.numberFormatter.stringFromNumber(self!.calibratedAcceleration.x + data.acceleration.x)
+            self!.yValueLabel.text = self?.numberFormatter.stringFromNumber(self!.calibratedAcceleration.x + data.acceleration.y)
+            self!.zValueLabel.text = self?.numberFormatter.stringFromNumber(self!.calibratedAcceleration.x + data.acceleration.z)
+            
+            if self?.functionalitySwitch.on != nil {
+                self?.setBackgroundWithCalibration()
+            } else if self?.view.backgroundColor != UIColor.whiteColor() {
+                self?.view.backgroundColor = UIColor.whiteColor()
+            }
+        }
     }
-    */
 
+    @IBAction func zeroButtonTapped(sender: AnyObject) {
+        self.calibratedAcceleration.x = self.motionManager.accelerometerData.acceleration.x
+        self.calibratedAcceleration.y = self.motionManager.accelerometerData.acceleration.y
+        self.calibratedAcceleration.z = self.motionManager.accelerometerData.acceleration.z
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+    
+    func setBackgroundWithCalibration() {
+        self.view.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    }
 }
